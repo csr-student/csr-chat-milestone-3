03c0dd48e6b75d6451625448c6b78c62b65a6f4a
Cody Sugarman <codyjsugarman@codys-macbook-pro.local>
2019-06-19 22:59:01 -0400

    Initial Commit

---
+++ csr-chat-milestone-3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  csr-chat-milestone-3
+//
+//  Created by Cody Sugarman on 6/19/19.
+//  Copyright © 2019 Cody Sugarman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
